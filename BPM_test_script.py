6efa8124a89be90371c914f568185ed77c66ef8a
Charlie <charliex@slac.stanford.edu>
2016-09-22 12:53:33 -0700

    Adding features
    Adding save or not save option for record.
    Adding characterizing another BPM option.

--- BPM_test_script.py
+++ BPM_test_script.py
@@ -4,5 +4,12 @@ Instrumentation and Controls Division
 Date: 03/18/2013
 author: Chengcheng Xu
 '''
-from NWA_src import *
-BPM = BPM_chara()
+from NWA_src1 import *
+i = 1
+while i == 1:
+	BPM = BPM_chara()
+	redo_opt = raw_input("Would you like to characterize another BPM?\n---> ")
+	if (redo_opt == "YES") or (redo_opt == "YEs") or (redo_opt == "Yes") or (redo_opt == "yes") or (redo_opt == "Y") or (redo_opt == "y"):
+		i = 1
+	else:
+		i = 0
